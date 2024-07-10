15b679b793d99af59f22b1fddeb80c91f00ecc47
KellyAllen013069 <89815777+KellyAllen013069@users.noreply.github.com>
2024-07-10 14:48:17 -0500

    exercise 2 complete

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -2,9 +2,31 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite subject?");
+            string answer = Console.ReadLine().ToLower();
+            switch (answer)
+            {
+                case "math":
+                    Console.WriteLine("Math rocks!!!");
+                    break;
+                case "history":
+                    Console.WriteLine("Nice! History is very interesting.");
+                    break;
+                case "science":
+                    Console.WriteLine("Great! Science is great for research.");
+                    break;
+                case "english":
+                    Console.WriteLine("Perfect! English will help you communicate effectively.");
+                    break;
+                case "dance":
+                    Console.WriteLine("Fantastic! Dance is the best form of exercise.");
+                    break;
+                default:
+                    Console.WriteLine($"You entered {answer}.");
+                    break;
+            }
         }
     }
 }
